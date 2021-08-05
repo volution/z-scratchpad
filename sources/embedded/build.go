15a2c27760583bccb69d2c6a02ecafdfc91445b6
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-08-05 19:58:57 +0300

    [sources]  Add embedded build information.

---
+++ sources/embedded/build.go
@@ -0,0 +1,22 @@
+
+
+package embedded
+
+
+import _ "embed"
+
+
+
+
+//go:embed build/version.txt
+var BuildVersion string
+
+//go:embed build/number.txt
+var BuildNumber string
+
+//go:embed build/timestamp.txt
+var BuildTimestamp string
+
+//go:embed build/sources-md5.txt
+var BuildSourcesMd5 string
+
