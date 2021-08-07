6a50027e95dcc7681c3139e0ebf1099e014ee43c
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-08-07 21:27:27 +0300

    [sources]  Add simple random token generation.

--- sources/lib/crypto.go
+++ sources/lib/crypto.go
@@ -4,6 +4,7 @@ package zscratchpad
 
 
 import "crypto/sha256"
+import crand "crypto/rand"
 import "encoding/hex"
 
 
@@ -17,5 +18,19 @@ func fingerprintBytes (_value []byte) (string) {
 	_hasher := sha256.New ()
 	_hasher.Write (_value)
 	return hex.EncodeToString (_hasher.Sum (nil))
+}
+
+
+func generateRandomToken () (string) {
+	var _data [128 / 8]byte
+	if _read, _error := crand.Read (_data[:]); _error == nil {
+		if _read != (128 / 8) {
+			panic (0x57a9916a)
+		}
+	} else {
+		panic (0xb6800787)
+	}
+	_token := hex.EncodeToString (_data[:])
+	return _token
 }
 
