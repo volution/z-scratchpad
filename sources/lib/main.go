ada7b09d1a58c48ae275b41d9b3dc36f17633db1
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-08-12 16:24:03 +0300

    [sources]  If searching returns multiple options (and mulitple is not allowed), show another select menu.

--- sources/lib/main.go
+++ sources/lib/main.go
@@ -533,7 +533,16 @@ func MainSearch (_flags *SearchFlags, _globals *Globals, _index *Index, _editor 
 					// NOP
 				default :
 					if ! flagBoolOrDefault (_flags.MultipleAllowed, false) {
-						return errorw (0xea0431aa, nil)
+						// FIXME:  Use document titles instead of identifiers!
+						_options := make ([][2]string, 0, len (_selection))
+						for _, _selection := range _selection {
+							_identifier := _selection[1]
+							_options = append (_options, [2]string { _identifier, _identifier })
+						}
+						_selection, _error = mainListSelect (_options, _editor)
+						if _error != nil {
+							return _error
+						}
 					}
 			}
 			for _, _selection := range _selection {
