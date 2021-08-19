f359945f77e871788d22963ee35172f47694a83f
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-08-19 20:50:38 +0300

    [sources]  Optimize `crypto`.

--- sources/lib/crypto.go
+++ sources/lib/crypto.go
@@ -3,48 +3,64 @@
 package zscratchpad
 
 
-import "crypto/sha256"
+import "crypto/sha1"
 import crand "crypto/rand"
 import "encoding/hex"
-import "io"
+import "hash"
+import "runtime"
 
 
 
 
 func fingerprintString (_value string) (string) {
-	return fingerprintBytes ([]byte (_value))
+	_fingerprint := fingerprintBytes (StringToBytes (_value))
+	runtime.KeepAlive (_value)
+	return _fingerprint
 }
 
 func fingerprintBytes (_value []byte) (string) {
-	_hasher := sha256.New ()
+	_hasher := sha1.New ()
 	_hasher.Write (_value)
-	return hex.EncodeToString (_hasher.Sum (nil))
+	return fingerprintFinalize (_hasher)
 }
 
 
 func fingerprintStringLines (_values []string) (string) {
-	_hasher := sha256.New ()
+	_hasher := sha1.New ()
 	_separator := []byte { '\n' }
 	for _, _value := range _values {
-		io.WriteString (_hasher, _value)
+		_hasher.Write (StringToBytes (_value))
+		runtime.KeepAlive (_value)
 		_hasher.Write (_separator)
 	}
-	return hex.EncodeToString (_hasher.Sum (nil))
+	return fingerprintFinalize (_hasher)
+}
+
+
+func fingerprintFinalize (_hasher hash.Hash) (string) {
+	var _data_0 [sha1.Size]byte
+	_data := NoEscapeBytes (_data_0[:])
+	_hasher.Sum (_data[:0])
+	_fingerprint := hex.EncodeToString (_data)
+	runtime.KeepAlive (_data_0)
+	return _fingerprint
 }
 
 
 
 
 func generateRandomToken () (string) {
-	var _data [128 / 8]byte
-	if _read, _error := crand.Read (_data[:]); _error == nil {
+	var _data_0 [128 / 8]byte
+	_data := NoEscapeBytes (_data_0[:])
+	if _read, _error := crand.Read (_data); _error == nil {
 		if _read != (128 / 8) {
 			panic (0x57a9916a)
 		}
 	} else {
 		panic (0xb6800787)
 	}
-	_token := hex.EncodeToString (_data[:])
+	_token := hex.EncodeToString (_data)
+	runtime.KeepAlive (_data_0)
 	return _token
 }
 
