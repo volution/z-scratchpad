29199f67363bf2cc5919caa13c4f82fceaf4d9c8
Ciprian Dorin Craciun <ciprian@volution.ro>
2021-09-13 02:19:52 +0300

    [assets]  Enhance clipboard JavaScript module.

--- sources/embedded/assets/js/clipboard.js
+++ sources/embedded/assets/js/clipboard.js
@@ -15,47 +15,119 @@
 	
 	function _initialize () {
 		
-		for (let _target of document.querySelectorAll ("main.document :not(pre) code")) {
-			_target.addEventListener ("click", (_event) => {
-					if (_event.detail == 2) {
-						_handle (_target, _event);
-					}
-				});
+		for (let _target of document.querySelectorAll ([
+				"html:root > body > header > h1",
+				"html:root > body > header code",
+				"html:root > body > header time",
+		].join (", "))) {
+			_register (_target, 1);
 		}
-		for (let _target of document.querySelectorAll ("main.document code")) {
-			_target.addEventListener ("click", (_event) => {
-					if (_event.detail == 2) {
-						_handle (_target, _event);
-					}
-				});
-		}
-		for (let _target of document.querySelectorAll ("main.document > *")) {
-			_target.addEventListener ("click", (_event) => {
-					if (_event.detail == 3) {
-						_handle (_target, _event);
-					}
-				});
+		for (let _target of document.querySelectorAll ([
+				"main.document:not(.document-format-text) h1",
+				"main.document:not(.document-format-text) h2",
+				"main.document:not(.document-format-text) h3",
+				"main.document:not(.document-format-text) h4",
+				"main.document:not(.document-format-text) h5",
+				"main.document:not(.document-format-text) h6",
+				"main.document:not(.document-format-text) p",
+				"main.document:not(.document-format-text) ul",
+				"main.document:not(.document-format-text) ol",
+				"main.document:not(.document-format-text) dl",
+				"main.document:not(.document-format-text) li",
+				"main.document:not(.document-format-text) dt",
+				"main.document:not(.document-format-text) dd",
+				"main.document:not(.document-format-text) blockquote",
+				"main.document:not(.document-format-text) pre",
+				"main.document:not(.document-format-text) :not(pre) > code",
+		].join (", "))) {
+			_register (_target, 5);
 		}
 	}
 	
 	
 	
 	
-	function _handle (_target, _event) {
-		_copy (_target);
+	function _handle (_target, _event, _expectedClicks) {
+		
+		if (_target !== _currentTarget) {
+			if (_currentTarget !== null) {
+				_currentTarget.classList.remove ("clipboard-active");
+				_currentTarget.classList.remove ("clipboard-copied");
+			}
+			_currentTarget = _target;
+			_currentClicks = 1;
+		} else {
+			_currentClicks += 1;
+		}
+		
+		if (_event.button == 1) {
+			if ((_currentClicks == 1) && (_currentClicks < _expectedClicks)) {
+				_currentClicks = _expectedClicks - 1;
+			}
+		}
+		
+		if (_currentClicks < _expectedClicks) {
+			
+			if (_currentClicks == (_expectedClicks - 1)) {
+				_currentTarget.classList.add ("clipboard-active");
+			}
+			
+			if (_currentTimeout !== null) {
+				window.clearTimeout (_currentTimeout);
+			}
+			_currentTimeout = window.setTimeout (() => {
+					_currentTarget.classList.remove ("clipboard-active");
+					_currentTarget = null;
+					_currentClicks = 0;
+					_currentTimeout = null;
+				}, 500);
+			
+		} else if (_currentClicks == _expectedClicks) {
+			
+			_copy (_target);
+			
+			_currentTarget.classList.remove ("clipboard-active");
+			_currentTarget.classList.add ("clipboard-copied");
+			if (_currentTimeout !== null) {
+				window.clearTimeout (_currentTimeout);
+			}
+			_currentTimeout = window.setTimeout (() => {
+					_currentTarget.classList.remove ("clipboard-copied");
+					_currentTarget = null;
+					_currentClicks = 0;
+					_currentTimeout = null;
+				}, 6000);
+		}
+		
 		_event.stopPropagation ();
 		_event.preventDefault ();
 	}
+	
+	function _register (_target, _expectedClicks) {
+		_target.classList.add ("clipboard-target");
+		_target.addEventListener ("click", (_event) => {
+				_handle (_target, _event, _expectedClicks);
+			});
+		_target.addEventListener ("auxclick", (_event) => {
+				_handle (_target, _event, _expectedClicks);
+			});
+	}
+	
+	var _currentTarget = null;
+	var _currentClicks = 0;
+	var _currentTimeout = null;
 	
 	function _copy (_source) {
 		
 		var _selection = window.getSelection ();
 		_selection.removeAllRanges ();
+		
 		var _range = document.createRange ();
 		_range.selectNodeContents (_source);
 		_selection.addRange (_range);
 		
 		var _selectionText = _selection.toString ();
+		_selection.removeAllRanges ();
 		
 		if (true) {
 			var _selectionEncoded = _selectionText;
